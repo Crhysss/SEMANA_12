d8ed61229bee97123d7d25e1f27d08ea48862bd4
Crhysss <crhystel.velasco@udla.edu.ec>
2023-06-19 15:39:46 -0500

    bucle for para pedir los datos

--- trabajo_en_clase.c
+++ trabajo_en_clase.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-struct alumno{
+struct alumno// no es una variable es un tipo de dato
+{
     char nombre[30];
     char direccion[50];
     char carrera[30];
@@ -7,5 +8,18 @@ struct alumno{
     float promedio;
 };
 int main(){
-    
+    struct alumno datos[5];
+    int i=0;
+    fputs("Ingrese los datos del alumno\n", stdout);
+    for(i=0;i<5;i++){
+        printf("Alumno %d\n", i+1);
+        fputs("Nombre\n", stdout);
+        gets(datos[i].nombre);
+        fputs("Direccion\n", stdout);
+        gets(datos[i].direccion);
+        fputs("Carrera\n", stdout);
+        gets(alumnos[i].carrera);
+        fputs("Edad\n", stdout);
+        
+    }
 }
